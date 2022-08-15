d86fa16daaf035bff82a395045f20d9308c7783c
princeerq98 <princeerq98@gmail.com>
2022-08-15 15:15:44 +0800

    chat app deploy

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+const projectID = '33556506-b14f-4471-b325-cb0e85bc188c';
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <div>
+      <ChatEngine
+        height="100vh"
+        projectID={projectID}
+        userName={localStorage.getItem('username')}
+        userSecret={localStorage.getItem('password')}
+        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+        onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+      />
+    </div>
+  );
+};
+
+// infinite scroll, logout, more customizations...
+
+export default App;
